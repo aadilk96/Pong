48cadb82ab3a9d4ae6535aea1dbf2cf89d50a808
LennixM <lennartmeyer@Lennarts-MacBook-Pro.local>
2018-10-20 00:37:30 +0200

    ...

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/getFriends', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    var users = User.findById().
 });
 
 router.post('/newUser', function(req, res) {
@@ -21,3 +21,29 @@ app.use('/api', router);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'path/to/database.sqlite',
+
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
+
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync();
